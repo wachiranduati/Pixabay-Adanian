8b6c33a0ed500f78e1990a62afeb856c9b0b65f2
Wachira Nduati <wachiranduati@gmail.com>
2021-12-13 16:50:55 +0300

    create the Image Details Fragment

---
+++ app/src/main/java/com/example/pixabay_adanian/ui/ImageDetailsFragment.kt
@@ -0,0 +1,19 @@
+package com.example.pixabay_adanian.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.pixabay_adanian.R
+
+class ImageDetailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_image_details, container, false)
+    }
+}
