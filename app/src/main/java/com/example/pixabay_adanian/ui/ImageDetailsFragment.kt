b5874c1cc554e0bea5e7d3e5a6875dcbf2f00874
Wachira Nduati <wachiranduati@gmail.com>
2021-12-14 13:27:37 +0300

    testing aborted

--- app/src/main/java/com/example/pixabay_adanian/ui/ImageDetailsFragment.kt
+++ app/src/main/java/com/example/pixabay_adanian/ui/ImageDetailsFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.pixabay_adanian.R
 import com.example.pixabay_adanian.models.Hit
--- app/src/main/java/com/example/pixabay_adanian/ui/MainScreenFragment.kt
+++ app/src/main/java/com/example/pixabay_adanian/ui/MainScreenFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
